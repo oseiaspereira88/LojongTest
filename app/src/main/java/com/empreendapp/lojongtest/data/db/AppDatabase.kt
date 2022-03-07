5902e167928f254337de62da5692c637f47accd5
Oseias Pereira <oseiaspereira88@gmail.com>
2022-03-07 00:45:02 -0300

    Define o AppDatabase

---
+++ app/src/main/java/com/empreendapp/lojongtest/data/db/AppDatabase.kt
@@ -0,0 +1,43 @@
+package com.empreendapp.lojongtest.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.empreendapp.lojongtest.data.db.dao.StepDao
+import com.empreendapp.lojongtest.data.db.entity.StepEntity
+import com.empreendapp.lojongtest.data.util.Converters
+
+@Database(entities = [StepEntity::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun stepDao(): StepDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(ctx: Context): AppDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) return tempInstance
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    ctx.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                )
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+
+    }
+
+}
