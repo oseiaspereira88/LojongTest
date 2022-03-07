c0489c131bcd32cdeb65d41c4cf3fdc88fcb0af2
Oseias Pereira <oseiaspereira88@gmail.com>
2022-03-07 00:40:34 -0300

    Define o model

---
+++ app/src/main/java/com/empreendapp/lojongtest/model/Step.kt
@@ -0,0 +1,32 @@
+package com.empreendapp.lojongtest.model
+
+import com.empreendapp.lojongtest.data.db.entity.StepEntity
+import java.util.*
+
+data class Step(
+    internal val id: Long,
+    var text: String,
+    internal val v: Int,
+    internal val source: String,
+    internal val updatedAt: Date,
+    internal val type: String,
+    internal val createdAt: Date,
+    internal val deleted: Boolean,
+    internal val used: Boolean,
+    internal val stepStatus: StepStatus
+)
+
+fun StepEntity.toStep(): Step {
+    return Step(
+        id = this.id,
+        text = this.text,
+        v = this.v,
+        source = this.source,
+        updatedAt = this.updatedAt,
+        type = this.type,
+        createdAt = this.createdAt,
+        deleted = this.deleted,
+        used = this.used,
+        stepStatus = this.stepStatus
+    )
+}
---
+++ app/src/main/java/com/empreendapp/lojongtest/model/StepStatus.kt
@@ -0,0 +1,3 @@
+package com.empreendapp.lojongtest.model
+
+class StepStatus(var verified: Boolean, var sendCount: Int)
